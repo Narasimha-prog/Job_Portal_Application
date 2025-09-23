10883111348751b0ef2ef822c7e8b1478a7acebf
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2025-09-23 18:52:28 +0530

    intial commit

---
+++ Job_Portal_Application/src/test/java/lnr/Job_Portal_Application/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package lnr.Job_Portal_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
